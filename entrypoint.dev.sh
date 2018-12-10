55f5fb18ecc93cc2b44e9e42f49f937607ae47ef
thoritie <thohirh9@gmail.com>
2018-12-10 18:18:21 +0700

    Add static to gitignore and add uwsgi :zap:

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -2,4 +2,5 @@
 
 cd simplezat
 pipenv run python manage.py migrate --settings=simplezat.settings.dev
-pipenv run python manage.py runserver 0.0.0.0:8000 --settings=simplezat.settings.dev
+pipenv run python manage.py collecstatic --noinput --settings=simplezat.settings.dev
+pipenv run uwsgi --ini uwgsi.dev.ini
