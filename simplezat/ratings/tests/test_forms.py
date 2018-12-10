5794d01d074586b326eac899d95ed30c530301df
thoritie <thohirh9@gmail.com>
2018-12-10 15:00:12 +0700

    Add form and playbook admin deploy :zap:

---
+++ simplezat/ratings/tests/test_forms.py
@@ -0,0 +1,65 @@
+from django import forms
+from django.test import TestCase
+
+from ..forms import RatingForm
+from ..models import Rating
+
+
+class RatingFormTest(TestCase):
+    def setUp(self):
+        self.form = RatingForm()
+
+    def test_form_should_have_define_field(self):
+        expected_field = [
+            'sentiment',
+            'comment',
+        ]
+        for each in expected_field:
+            self.assertTrue(each in self.form.fields)
+        self.assertEqual(len(self.form.fields), 2)
+
+    def test_form_should_have_correct_field_and_widget(self):
+        self.assertIsInstance(
+            self.form.fields['sentiment'].widget,
+            forms.HiddenInput
+        )
+        self.assertIsInstance(
+            self.form.fields['comment'].widget,
+            forms.Textarea
+        )
+
+    def test_form_with_invalid_input_should_be_invalid(self):
+        data = {
+            'sentiment': 'positive',
+            'comment': '',
+        }
+        form = RatingForm(data=data)
+        self.assertFalse(form.is_valid())
+
+        expected = {
+            'comment': ['Please write somethings...']
+        }
+        self.assertEqual(form.errors, expected)
+
+    def test_form_with_valid_input_should_be_valid(self):
+        data = {
+            'sentiment': 'positive',
+            'comment': 'You did great!',
+        }
+
+        form = RatingForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_with_valid_input_should_be_save(self):
+        data = {
+            'sentiment': 'positive',
+            'comment': 'You did great!',
+        }
+
+        form = RatingForm(data=data)
+        form.is_valid()
+        form.save()
+
+        rating = Rating.objects.last()
+        self.assertEqual(rating.sentiment, 'positive')
+        self.assertEqual(rating.comment, 'You did great!')
--- tests/cypress/integration/simplezat/rating.spec.js
+++ tests/cypress/integration/simplezat/rating.spec.js
@@ -4,7 +4,7 @@ context('Rating', () => {
   })
 
   it('should have 3 rating', () =>{
-    cy.contains('How do we do?')
+    cy.get('h1').should('contain', 'How do we do?')
     cy.get('[href="/ratings/positive/"] > img').should('have.attr', 'alt', 'Positive')
     cy.get('[href="/ratings/neutral/"] > img').should('have.attr', 'alt', 'Neutral')
     cy.get('[href="/ratings/negative/"] > img').should('have.attr', 'alt', 'Negative')
@@ -12,11 +12,24 @@ context('Rating', () => {
     cy.get('img[alt="Positive"]').click()
     cy.wait(1000)
 
-    cy.contains('Any comments?')
+    cy.get('h1').should('contain', 'Any comments?')
     cy.get('textarea[name="comment"]').type('you are doing great!')
     cy.get('button').click()
     cy.wait(1000)
 
-    cy.contains('Thank You!')
+    cy.get.('h1').should('contain', 'Thank You!')
+  })
+})
+
+
+  it('shoud show error message when give rating without comment', () =>{
+    cy.get('img[alt="Positive"]').click()
+    cy.wait(1000)
+
+    cy.get('button').click()
+    cy.wait(1000)
+
+    cy.get('body').should('not.contain', 'Thank You!')
+    cy.get('p.error').should('contain', 'Please write somethings...')
   })
 })
