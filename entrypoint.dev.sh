ae0633ad00283de294be222cbeaf7c1b9562882a
thoritie <thohirh9@gmail.com>
2018-12-10 18:33:11 +0700

    Edit collectstatic :zap:

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -2,5 +2,5 @@
 
 cd simplezat
 pipenv run python manage.py migrate --settings=simplezat.settings.dev
-pipenv run python manage.py collecstatic --noinput --settings=simplezat.settings.dev
+pipenv run python manage.py collectstatic --noinput --settings=simplezat.settings.dev
 pipenv run uwsgi --ini uwgsi.dev.ini
--- entrypoint.production.sh
+++ entrypoint.production.sh
@@ -2,5 +2,5 @@
 
 cd simplezat
 pipenv run python manage.py migrate --settings=simplezat.settings.prod
-pipenv run python manage.py collecstatic --noinput --settings=simplezat.settings.prod
+pipenv run python manage.py collectstatic --noinput --settings=simplezat.settings.prod
 pipenv run uwsgi --ini uwgsi.prod.ini
